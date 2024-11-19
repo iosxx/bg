5e5daa9e890068718bfbbb5460b370f5b9db4f10
iosxx <51318333+iosxx@users.noreply.github.com>
2024-11-20 00:59:35 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/g0f/Notion-DoIt-143e7ebb7648807f908ac3ea50724a69?pvs=4',',
         pages: [
             // {
             //     page_id: '<page_id>',
